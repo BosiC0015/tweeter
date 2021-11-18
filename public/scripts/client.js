4fa40afe2155618b43e48a6c0c9cd154661a7b98
Bosi Chen <chenbosi.bosi@gmail.com>
2021-11-17 20:12:51 -0500

    get tweet from /tweets and display in container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,42 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = function(tweetObj) {
+  $('.container').append(
+  `<article class="tweet">
+  <header class="article-header">
+    <div class="article-avatar-name">
+      <div class="article-avatar">
+        <img src="${tweetObj.user.avatars}" height="70px" width="auto">
+      </div>
+      <div class="article-name">${tweetObj.user.name}</div>
+    </div>
+    <div class="handle">${tweetObj.user.handle}</div>
+  </header>
+  <div class="tweet-content">
+    ${tweetObj.content.text}
+  </div>
+  <footer class="article-footer">
+    <div>${timeago.format(tweetObj.created_at)}</div>
+    <div>
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </div>
+  </footer>
+</article>`);
+}
+
+// get tweet data from '/tweets' and append to the .container by passing it to createTweetElement()
+const renderTweets = function() {
+  $.get('/tweets').then(data => {
+    // console.log(data)
+    for (const tweetData of data) {
+      createTweetElement(tweetData);
+    }
+  })
+}
+
+$(() => {
+  renderTweets();
+});
